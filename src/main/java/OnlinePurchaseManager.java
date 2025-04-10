06d97e4160229b55ac5604bf1123e52462b855e6
featherfly12 <146142331+featherfly12@users.noreply.github.com>
2025-04-10 16:53:14 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -1,48 +1,51 @@
-import java.util.*;
+import java.util.ArrayList;
 
-public class GizmoList
-{
-  private ArrayList<Gizmo> inventory;
+public class OnlinePurchaseManager {
+    /** An ArrayList of purchased Gizmo objects, instantiated in the constructor. */
+    private ArrayList<Gizmo> purchases;
 
-  public GizmoList()
-  {
-    inventory = new ArrayList<Gizmo>();
-  }
-
-  public void addGizmo(Gizmo g)
-  {
-    inventory.add(g);
-  }
-
-  /** Returns the number of Gizmos in inventory by maker
-   *  that are electronic.
-   */
-  public int countElectronicsByMaker(String maker)
-  {
-    int count = 0;
-    for (Gizmo g : inventory)
-    {
-      if (g.isElectronic() && g.getMaker().equals(maker))
-      {
-        count++;
-      }
+    // Constructor to initialize the purchases list
+    public OnlinePurchaseManager() {
+        purchases = new ArrayList<Gizmo>();
     }
-    return count;
-  }
 
-  /** Returns true if any two Gizmos in inventory are
-   *  equivalent, but not the same object.
-   */
-  public boolean hasAdjacentEqualPair()
-  {
-    for (int i = 0; i < inventory.size() - 1; i++)
-    {
-      if (inventory.get(i).equals(inventory.get(i + 1)) &&
-          inventory.get(i) != inventory.get(i + 1))
-      {
-        return true;
-      }
+    /** Returns the number of purchased Gizmo objects that are electronic
+     * whose manufacturer is maker, as described in part (a).
+     */
+    public int countElectronicsByMaker(String maker) {
+        int count = 0;
+        for (Gizmo g : purchases) {
+            // Check if the Gizmo is electronic and made by the specified manufacturer
+            if (g.isElectronic() && g.getMaker().equals(maker)) {
+                count++;
+            }
+        }
+        return count;
     }
-    return false;
-  }
+
+    /** Returns true if any pair of adjacent purchased Gizmo objects are
+     * equivalent, and false otherwise, as described in part (b).
+     */
+    public boolean hasAdjacentEqualPair() {
+        // If there are fewer than two purchases, there can't be adjacent pairs
+        if (purchases.size() < 2) {
+            return false;
+        }
+
+        // Check each pair of adjacent Gizmo objects
+        for (int i = 0; i < purchases.size() - 1; i++) {
+            // If two adjacent Gizmos are equivalent, return true
+            if (purchases.get(i).equals(purchases.get(i + 1))) {
+                return true;
+            }
+        }
+
+        // If no adjacent equal pair is found, return false
+        return false;
+    }
+
+    // Additional methods to add Gizmos to the purchases list, etc.
+    public void addPurchase(Gizmo g) {
+        purchases.add(g);
+    }
 }
