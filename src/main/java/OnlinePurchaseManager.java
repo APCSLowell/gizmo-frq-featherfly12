761de4388a67beb30f69b80c25c0547bbb8c5168
featherfly12 <146142331+featherfly12@users.noreply.github.com>
2025-04-10 16:42:15 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -1,32 +1,48 @@
-import java.util.ArrayList;
-public class OnlinePurchaseManager
+import java.util.*;
+
+public class GizmoList
 {
-  /** An ArrayList of purchased Gizmo objects,
-  * instantiated in the constructor.
-  */
-  private ArrayList<Gizmo> purchases;
-  
-  /** Returns the number of purchased Gizmo objects that are electronic
-  * whose manufacturer is maker, as described in part (a).
-  */
+  private ArrayList<Gizmo> inventory;
+
+  public GizmoList()
+  {
+    inventory = new ArrayList<Gizmo>();
+  }
+
+  public void addGizmo(Gizmo g)
+  {
+    inventory.add(g);
+  }
+
+  /** Returns the number of Gizmos in inventory by maker
+   *  that are electronic.
+   */
   public int countElectronicsByMaker(String maker)
   {
-    /* to be implemented in part (a) */
+    int count = 0;
+    for (Gizmo g : inventory)
+    {
+      if (g.isElectronic() && g.getMaker().equals(maker))
+      {
+        count++;
+      }
+    }
+    return count;
   }
-  
-  /** Returns true if any pair of adjacent purchased Gizmo objects are
-  * equivalent, and false otherwise, as described in part (b).
-  */
+
+  /** Returns true if any two Gizmos in inventory are
+   *  equivalent, but not the same object.
+   */
   public boolean hasAdjacentEqualPair()
   {
-    /* to be implemented in part (b) */
-  }
-  public OnlinePurchaseManager()
-  {
-    purchases = new ArrayList <Gizmo>();
-  }
-  public void add(Gizmo g)
-  {
-    purchases.add(g);
+    for (int i = 0; i < inventory.size() - 1; i++)
+    {
+      if (inventory.get(i).equals(inventory.get(i + 1)) &&
+          inventory.get(i) != inventory.get(i + 1))
+      {
+        return true;
+      }
+    }
+    return false;
   }
 }
