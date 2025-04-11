f841bff246264ce2c68e4b36a39b65c15bb4ad81
featherfly12 <146142331+featherfly12@users.noreply.github.com>
2025-04-10 17:13:04 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -1,32 +1,41 @@
 import java.util.ArrayList;
- public class OnlinePurchaseManager
- {
-   /** An ArrayList of purchased Gizmo objects,
-   * instantiated in the constructor.
-   */
-   private ArrayList<Gizmo> purchases;
-   
-   /** Returns the number of purchased Gizmo objects that are electronic
-   * whose manufacturer is maker, as described in part (a).
-   */
-   public int countElectronicsByMaker(String maker)
-   {
-     /* to be implemented in part (a) */
-   }
-   
-   /** Returns true if any pair of adjacent purchased Gizmo objects are
-   * equivalent, and false otherwise, as described in part (b).
-   */
-   public boolean hasAdjacentEqualPair()
-   {
-     /* to be implemented in part (b) */
-   }
-   public OnlinePurchaseManager()
-   {
-     purchases = new ArrayList <Gizmo>();
-   }
-   public void add(Gizmo g)
-   {
-     purchases.add(g);
-   }
- }
+
+public class OnlinePurchaseManager {
+    /** An ArrayList of purchased Gizmo objects,
+    * instantiated in the constructor.
+    */
+    private ArrayList<Gizmo> purchases;
+    
+    /** Returns the number of purchased Gizmo objects that are electronic
+    * whose manufacturer is maker, as described in part (a).
+    */
+    public int countElectronicsByMaker(String maker) {
+        int count = 0;
+        for (Gizmo g : purchases) {
+            if (g.isElectronic() && g.getMaker().equals(maker)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    /** Returns true if any pair of adjacent purchased Gizmo objects are
+    * equivalent, and false otherwise, as described in part (b).
+    */
+    public boolean hasAdjacentEqualPair() {
+        for (int i = 0; i < purchases.size() - 1; i++) {
+            if (purchases.get(i).equals(purchases.get(i + 1))) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public OnlinePurchaseManager() {
+        purchases = new ArrayList<Gizmo>();
+    }
+    
+    public void add(Gizmo g) {
+        purchases.add(g);
+    }
+}
